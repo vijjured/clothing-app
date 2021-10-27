5d329a67e8b28543d2a8201f5a3e8853894657f3
Vijay Bhargav Reddy Chelamala <vijaybhargavreddychelamala@Vijays-MacBook-Pro.local>
2021-10-26 21:23:43 -0700

    Built initial skeleton for project and create homepage component

---
+++ src/components/homepage.component.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content"></div>
+        <h1 className="title">HATS</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+      <div className="menu-item">
+        <div className="content"></div>
+        <h1 className="title">JACKETS</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+      <div className="menu-item">
+        <div className="content"></div>
+        <h1 className="title">WOMENS</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+      <div className="menu-item">
+        <div className="content"></div>
+        <h1 className="title">MENS</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+      <div className="menu-item">
+        <div className="content"></div>
+        <h1 className="title">SNEAKERS</h1>
+        <span className="subtitle">Shop Now</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
